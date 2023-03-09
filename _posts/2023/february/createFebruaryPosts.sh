99a5dd8afbe4e0532c4966ea0f9f6459eaa9a7cc
ajones424 <ajones061392@cogswebdevelopmentandhosting.com>
2023-03-09 14:15:37 -0800

    Created the scripts to create the posts for each month. I plan on creating a CLI Tool for this to better help me manage and create the necessary files, and folders needed to keep up the blog.

---
+++ _posts/2023/february/createFebruaryPosts.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+
+
+echo "Creating February Post Files"
+touch 2023-02-12-blogtitle.md &&
+touch 2023-02-13-blogtitle.md  &&
+touch 2023-02-14-blogtitle.md  &&
+touch 2023-02-15-blogtitle.md  &&
+touch 2023-02-16-blogtitle.md  &&
+touch 2023-02-17-blogtitle.md  &&
+touch 2023-02-18-blogtitle.md  &&
+touch 2023-02-19-blogtitle.md  &&
+touch 2023-02-20-blogtitle.md  &&
+touch 2023-02-21-blogtitle.md  &&
+touch 2023-02-22-blogtitle.md  &&
+touch 2023-02-23-blogtitle.md  &&
+touch 2023-02-24-blogtitle.md &&
+touch 2023-02-25-blogtitle.md &&
+touch 2023-02-26-blogtitle.md &&
+touch 2023-02-27-blogtitle.md &&
+touch 2023-02-28-blogtitle.md
---
+++ scripts/project_setup/monthpostcreation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#Script: Month Post Creation
+#Description: The following script is to help create the posts for each of the following 
+#             months of the year.
+#
+
+echo 
